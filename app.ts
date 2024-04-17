aeaf572062723b41326fcb2a6fe8efb6488e9a27
A-M-I <160208713+Alisha000111@users.noreply.github.com>
2024-04-17 19:41:59 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,19 @@
+let count = 10;
+if (count > 0) {
+  console.log("Count is greater than 0.");
+}
+function add(a, b) {
+  return a + b;
+}
+console.log(
+    "Using a reserved word as a variable name causes a syntax error in JavaScript."
+  );
+async function fetchData() {
+    const data = await fetchSomething();
+    console.log(data);
+  }
+
+  console.log(
+    "The 'await' keyword allows asynchronous, promise-based behavior to be written in a cleaner, more linear fashion."
+  );
+  
